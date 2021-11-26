2f1bf790edd6b8d65dc61c178b605d35196bc773
Niko <59814096+s-webdev@users.noreply.github.com>
2021-11-27 05:14:10 +0800

    - changed error blade file title

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ $general->sitename($page_title ?? '500 | Internal Server Error') }}</title>
+  <title>500 | Internal Server Error</title>
   <link rel="shortcut icon" type="image/png" href="{{getImage(imagePath()['logoIcon']['path'] .'/favicon.png')}}">
   <!-- bootstrap 4  -->
   <link rel="stylesheet" href="{{ asset('assets/errors/css/bootstrap.min.css') }}">
@@ -30,6 +30,6 @@
   </div>
   <!-- error-404 end -->
 
-  
+
   </body>
 </html>
