0bbde0d731a87e4d51a7dcd99ae9bcb7034dcabb
Niko <59814096+s-webdev@users.noreply.github.com>
2021-11-29 14:40:31 +0800

    - updated for auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,23 +5,23 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Closure;
 use Auth;
+
 class Authenticate extends  Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
+     * @param array $guards
      * @return string|null
      */
 
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, ...$guards)
     {
         if (Auth::check()) {
             return $next($request);
         }
         return redirect()->route('user.login');
     }
-
-
-
 }
